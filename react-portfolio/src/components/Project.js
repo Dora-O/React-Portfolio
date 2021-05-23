2622d12f756316ab9edfcf2cf6f8539df62c6692
Dora O <dorozco2390@utexas.edu>
2021-05-22 21:42:33 -0500

    updated project sections

--- react-portfolio/src/components/Project.js
+++ react-portfolio/src/components/Project.js
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from "react";
 import { CardColumns, Card } from "react-bootstrap";
 
 import Code_Quiz from "../images/code-quiz.png";
@@ -20,69 +19,69 @@ function Project({ theme }) {
         <CardColumns>
               <Card >
                 <Card.Body>
-                  <a href="https://dora-o.github.io/Code_Quiz/" target="_blank">
+                  <a href="https://dora-o.github.io/Code_Quiz/" target="_blank" rel="noreferrer">
                     <Card.Title className="d-inline-block">
                       Code Quiz
                     </Card.Title>
                   </a>
                   <Card.Img variant="top" src={Code_Quiz} />
                 </Card.Body>
-                <a href="https://github.com/Dora-O/Code_Quiz" target="_blank">Code Quiz Repo</a>
+                <a href="https://github.com/Dora-O/Code_Quiz" target="_blank" rel="noreferrer">Code Quiz Repo</a>
               </Card>
               <Card >
                 <Card.Body>
-                  <a href="https://dora-o.github.io/PasswordGenerator/" target="_blank">
+                  <a href="https://dora-o.github.io/PasswordGenerator/" target="_blank" rel="noreferrer">
                     <Card.Title className="d-inline-block">
                       Password Generator
                     </Card.Title>
                   </a>
                   <Card.Img variant="top"src={Password_Generator}/>
                 </Card.Body>
-                <a href="https://github.com/Dora-O/PasswordGenerator" target="_blank">Password Generator Repo</a>
+                <a href="https://github.com/Dora-O/PasswordGenerator" target="_blank" rel="noreferrer">Password Generator Repo</a>
               </Card>
               <Card >
                 <Card.Body>
-                  <a href="https://manuelg139.github.io/Project_1/" target="_blank">
+                  <a href="https://manuelg139.github.io/Project_1/" target="_blank" rel="noreferrer">
                     <Card.Title className="d-inline-block">
                       Avengers Unite
                     </Card.Title>
                   </a>
                   <Card.Img variant="top" src={Avengers_Unite}/>
                 </Card.Body>
-                <a href="https://github.com/manuelg139/Project_1" target="_blank">Avengers Unite Repo</a>
+                <a href="https://github.com/manuelg139/Project_1" target="_blank" rel="noreferrer">Avengers Unite Repo</a>
               </Card>
               <Card >
                 <Card.Body>
-                  <a href="https://enigmatic-dawn-65751.herokuapp.com/" target="_blank">
+                  <a href="https://enigmatic-dawn-65751.herokuapp.com/" target="_blank" rel="noreferrer">
                     <Card.Title className="d-inline-block">
                       Workout-Tracker
                     </Card.Title>
                   </a>
                   <Card.Img variant="top" src={Workout_Tracker} />
                 </Card.Body>
-                <a href="https://github.com/Dora-O/Workout-Tracker" target="_blank">Workout-Tracker Repo</a>
+                <a href="https://github.com/Dora-O/Workout-Tracker" target="_blank" rel="noreferrer">Workout-Tracker Repo</a>
               </Card>
               <Card >
                 <Card.Body>
-                  <a href="https://hidden-reaches-62484.herokuapp.com/" target="_blank">
+                  <a href="https://hidden-reaches-62484.herokuapp.com/" target="_blank" rel="noreferrer">
                     <Card.Title className="d-inline-block">
                       Premiere Show
                     </Card.Title>
                   </a>
                   <Card.Img variant="top" src={Premiere_Show}/>
                 </Card.Body>
-                <a href="https://github.com/Dora-O/Project-2" target="_blank">Premiere Show Repo</a>
+                <a href="https://github.com/Dora-O/Project-2" target="_blank" rel="noreferrer">Premiere Show Repo</a>
               </Card>
               <Card >
                 <Card.Body>
-                  <a href="https://dora-o.github.io/Employee-Directory/" target="_blank">
+                  <a href="https://dora-o.github.io/Employee-Directory/" target="_blank" rel="noreferrer">
                     <Card.Title className="d-inline-block">
                       Employee Directory
                     </Card.Title>
                   </a>
                   <Card.Img variant="top" src={Employee_Directory} />
                 </Card.Body>
-                <a href="https://github.com/Dora-O/Employee-Directory" target="_blank">Employee Directory Repo</a>
+                <a href="https://github.com/Dora-O/Employee-Directory" target="_blank" rel="noreferrer">Employee Directory Repo</a>
               </Card>
             
           
