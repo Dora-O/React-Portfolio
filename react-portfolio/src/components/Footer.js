0d1cf5473cf1e94c111bfae350bc7ea57ae3c9e2
Dora O <dorozco2390@utexas.edu>
2021-05-22 21:06:29 -0500

    added footer

---
+++ react-portfolio/src/components/Footer.js
@@ -0,0 +1,11 @@
+function Footer(){
+  return (
+    <footer className="d-flex flex-column justify-content-center align-items-center bg-dark">
+      <p className="lead my-3 text-white">
+        &copy; Made with <span>♥️</span> by Dora Orozco
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
