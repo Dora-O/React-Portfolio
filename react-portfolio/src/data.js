6649295ed6aedca48d3065da8e2c066f839fd6df
Dora O <dorozco2390@utexas.edu>
2021-05-22 21:40:29 -0500

    added file for skill data

---
+++ react-portfolio/src/data.js
@@ -0,0 +1,43 @@
+import {
+    FaHtml5,
+    FaCss3Alt,
+    FaNpm,
+    FaGitAlt,
+    FaGithubSquare,
+  } from "react-icons/fa";
+  import { SiJavascript } from "react-icons/si";
+  
+
+  export const skillData = [
+    {
+      id: 1,
+      skill: <FaHtml5 className="display-4" />,
+      name: "HTML5",
+    },
+    {
+      id: 2,
+      skill: <FaCss3Alt className="display-4" />,
+      name: "CSS3",
+    },
+    {
+      id: 3,
+      skill: <SiJavascript className="display-4" />,
+      name: "JavaScript",
+    },
+    {
+      id: 7,
+      skill: <FaNpm className="display-4" />,
+      name: "npm",
+    },
+    {
+      id: 8,
+      skill: <FaGitAlt className="display-4" />,
+      name: "Git",
+    },
+    {
+      id: 9,
+      skill: <FaGithubSquare className="display-4" />,
+      name: "GitHub",
+    },
+  ];
+  
