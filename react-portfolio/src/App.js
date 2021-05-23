1ae2234188e5caa1d44aaa486c3536e3c7e71387
Dora O <dorozco2390@utexas.edu>
2021-05-22 21:22:37 -0500

    updated app.js

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "../src/components/About";
+import Skills from "../src/components/Skills";
+import Project from "../src/components/Project";
+import Contact from "../src/components/Contact";
+import Footer from "../src/components/Footer";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div>
+          <About />
+          <Skills />
+          <Project />
+          <Contact />
+          <Footer />
+      </div>
+  )
+};
 
 export default App;
